dd29f8a0676a66b61e2acea5b37da6af008ae17b
Cam <camlatimer@gmail.com>
2016-02-18 14:52:53 -0500

    started psuedo'ing

--- hangman.js
+++ hangman.js
@@ -1,1 +1,17 @@
 console.log("welcome to hangman");
+/*
+word = [];
+guesses = [];
+incorrectGuesses = 0;
+prompt user for a word
+  function() {
+  var word = prompt('gimme a word');
+}
+
+display the word to the user. represent unguessed letters with a '-'
+  represent correclty guessed letters with a letter that the the user guessed
+  function display() {
+    take word from user and make an array / string with -'s
+  }
+
+*/
