da390b881db3a9d79c553660f1be77cafce6cd0a
Cam <camlatimer@gmail.com>
2016-02-18 16:00:42 -0500

    commit 1

--- hangman.js
+++ hangman.js
@@ -1,17 +1,141 @@
 console.log("welcome to hangman");
-/*
-word = [];
-guesses = [];
-incorrectGuesses = 0;
-prompt user for a word
-  function() {
-  var word = prompt('gimme a word');
-}
 
-display the word to the user. represent unguessed letters with a '-'
-  represent correclty guessed letters with a letter that the the user guessed
-  function display() {
-    take word from user and make an array / string with -'s
+game = {
+  word: [],
+  displayWord: [],
+  guesses: [],
+  incorrectGuesses: 0,
+  getWord: function() {
+    
+  },
+  getLetter: function() {
+    var usrL = prompt('gimme a letter');
+    this.guesses.push(usrL);
+    return usrL;
+  },
+  showWord: function () {
+    //take game word and make an array / string with -'s
+    //this.word --> displayWord;
+    this.displayWord = this.word;
+    this.displayWord = this.displayWord.fill('-', 0, this.word.length);
+    this.displayWord = this.displayWord.join('');
+  },
+  getGuesses: function() {
+    this.guesses.forEach(function(a) {
+      if
+    })
+    var check = this.word.indexOf();
+    return check;
+  },
+  playGame: function() {
+    this.getWord();
+    this.showWord();
+
+  }
+};
+
+game.playGame();
+
+
+
+/*
+make an object
+  game = {};
+  game.word = [];
+  game.guesses = [];
+  game.incorrectGuesses = 0;
+
+  prompt user for a word
+    this.getWord = function() {
+    var word = prompt('gimme a word');
+  }
+  prompt user for a guess
+    this.getGuess = function() {
+    var guess = prompt('gimme a letter');
+    this.guesses = this.guesses.push(guess);
   }
 
+  method to display the word to the user. represent unguessed letters with a '-'
+  represent correclty guessed letters with a letter that the the user guessed
+  this.showWord = function display() {
+    take word from user and make an array / string with -'s
+    this.word --> displayWord;
+  }
+
+  method to check if guess is inside guesses
+    this.checkGuesses = function() {
+    prompt user for a guess
+      this.getGuess = function() {
+      var guess = prompt('gimme a letter');
+    }
+  }
+*/
+
+
+/*
+VERSION1 CODE
+var word = prompt('gimme a word:');
+var guesses = [];
+var attempts = word.length + 4;
+var attemptsLeft = attempts;
+var display = '';
+
+myl = 'a';
+place = word.indexOf(myl);
+
+function showDisplay() {
+  for(i = 0; i < word.length; i++) {
+    display += '-';
+  }
+  display;
+}
+function getGuess() {
+  if(display === word) {
+    attemptsLeft = 0;
+    win();
+  } else {
+    while (attemptsLeft > 0) {
+      var guess = prompt('You have ' + attemptsLeft + ' attempts. ' + 'guess a letter:\n' + display);
+      var place = word.indexOf(guess);
+      if (place > -1) {
+          attemptsLeft--;
+          display = display.replace(display[place], guess);
+          getGuess();
+
+      } else if (place < 0) {
+        attemptsLeft--;
+        if(attemtps == 0) {
+          lose();
+        }
+        }
+        getGuess();
+      }
+    }
+}
+
+function win() {
+    var playAgain = prompt('you win! play again? (y/n)');
+    if (playAgain == 'y') {
+    gameOn();
+    } else if (playAgain == 'n') {
+      alert('thanks for playing!');
+      return;
+    }
+}
+  function lose() {
+    var playAgain = prompt('you have no guesses left. play again? (y/n)');
+    if (playAgain == 'y') {
+      attemptsLeft = attempts;
+      gameOn();
+    } else if (playAgain == 'n') {
+      alert('thanks for playing!');
+      return;
+    }
+  }
+
+
+function gameOn() {
+  showDisplay();
+  getGuess();
+}
 */
